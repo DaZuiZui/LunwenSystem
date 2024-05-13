7a57ee2f6f1b8a15bf05ddbe3429751c2667a2e3
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2024-05-13 11:14:15 +0800

    1

---
+++ springboot_example-main/src/main/java/com/gsxy/filesystemserver/dao/AmsAwardDao.java
@@ -0,0 +1,85 @@
+package com.gsxy.filesystemserver.dao;
+
+import com.gsxy.filesystemserver.entity.AmsAward;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.data.domain.Pageable;
+import java.util.List;
+
+/**
+ * (AmsAward)表数据库访问层
+ *
+ * @author makejava
+ * @since 2024-05-13 11:08:19
+ */
+@Mapper
+public interface AmsAwardDao {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    AmsAward queryById(Integer id);
+
+    /**
+     * 查询指定行数据
+     *
+     * @param amsAward 查询条件
+     * @param pageable         分页对象
+     * @return 对象列表
+     */
+    List<AmsAward> queryAllByLimit(AmsAward amsAward, @Param("pageable") Pageable pageable);
+
+    /**
+     * 统计总行数
+     *
+     * @param amsAward 查询条件
+     * @return 总行数
+     */
+    long count(AmsAward amsAward);
+
+    /**
+     * 新增数据
+     *
+     * @param amsAward 实例对象
+     * @return 影响行数
+     */
+    int insert(AmsAward amsAward);
+
+    /**
+     * 批量新增数据（MyBatis原生foreach方法）
+     *
+     * @param entities List<AmsAward> 实例对象列表
+     * @return 影响行数
+     */
+    int insertBatch(@Param("entities") List<AmsAward> entities);
+
+    /**
+     * 批量新增或按主键更新数据（MyBatis原生foreach方法）
+     *
+     * @param entities List<AmsAward> 实例对象列表
+     * @return 影响行数
+     * @throws org.springframework.jdbc.BadSqlGrammarException 入参是空List的时候会抛SQL语句错误的异常，请自行校验入参
+     */
+    int insertOrUpdateBatch(@Param("entities") List<AmsAward> entities);
+
+    /**
+     * 修改数据
+     *
+     * @param amsAward 实例对象
+     * @return 影响行数
+     */
+    int update(AmsAward amsAward);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 影响行数
+     */
+    int deleteById(Integer id);
+
+}
+
---
+++ springboot_example-main/src/main/java/com/gsxy/filesystemserver/service/AmsAwardService.java
@@ -0,0 +1,56 @@
+package com.gsxy.filesystemserver.service;
+
+import com.gsxy.filesystemserver.entity.AmsAward;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+/**
+ * (AmsAward)表服务接口
+ *
+ * @author makejava
+ * @since 2024-05-13 11:08:19
+ */
+public interface AmsAwardService {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    AmsAward queryById(Integer id);
+
+    /**
+     * 分页查询
+     *
+     * @param amsAward 筛选条件
+     * @param pageRequest      分页对象
+     * @return 查询结果
+     */
+    Page<AmsAward> queryByPage(AmsAward amsAward, PageRequest pageRequest);
+
+    /**
+     * 新增数据
+     *
+     * @param amsAward 实例对象
+     * @return 实例对象
+     */
+    AmsAward insert(AmsAward amsAward);
+
+    /**
+     * 修改数据
+     *
+     * @param amsAward 实例对象
+     * @return 实例对象
+     */
+    AmsAward update(AmsAward amsAward);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 是否成功
+     */
+    boolean deleteById(Integer id);
+
+}
